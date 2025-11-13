c6d1e48dcef2c0355bb906f9acf6ca9cca563429
poszasz <156089572+poszasz@users.noreply.github.com>
2025-11-13 13:21:24 +0100

    Add files via upload

---
+++ Halloween-main/WpfApp4/ScoreRowUC.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp4.Networking;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for ScoreRowUC.xaml
+    /// </summary>
+    public partial class ScoreRowUC : UserControl
+    {
+        public ScoreRowUC()
+        {
+            InitializeComponent();
+           
+        }
+    }
+}
---
+++ Halloween-main/WpfApp4/StartWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp4.Models;
+using WpfApp4.Networking;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+            belepes.Click += (_, __) =>
+            {
+                string n = nev.Text.Trim().Replace('/', '-').Replace(' ', '-');
+                Jatek jatek =API.StartGame(n);
+                Game G = new Game(jatek);
+                G.Show();
+                this.Close();
+            };
+        } 
+    }
+}
